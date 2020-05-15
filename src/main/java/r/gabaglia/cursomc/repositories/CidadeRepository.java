52eb842a586f7b2e7593ddff8e565b0b3ac5a25e
Andre Raja <andre.raja@gmail.com>
2020-05-15 21:43:52 +0100

    Estado and Cidade.

---
+++ src/main/java/r/gabaglia/cursomc/repositories/CidadeRepository.java
@@ -0,0 +1,11 @@
+package r.gabaglia.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import r.gabaglia.cursomc.domain.Cidade;
+
+@Repository
+public interface CidadeRepository extends JpaRepository<Cidade, Integer> {
+
+}
